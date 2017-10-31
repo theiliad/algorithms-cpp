8e79d2cd4a1636c7f99f26a27bff425a57f9b0f8
The Iliad <iliadmoosavi@gmail.com>
2017-10-31 18:53:10 -0400

    Keep iterating until i & j need to be exchanged

--- quicksort.cpp
+++ quicksort.cpp
@@ -13,9 +13,39 @@ void swap(int* a, int* b) {
     *b = *a;
 }
 
+int partition(std::vector<int> a, int low, int high) {
+    // for (int i = 1; i < a.size(); i++) {
+    //     cout << "\ni: " << (a[low] > a[i]) << "\n";
+
+    //     // for (int j = arrayToBeSorted.size() - 1; j > 1; j--) {
+    //     //     cout << "\ni: " << i << "\n";
+    //     // }
+    // }
+    int i = 1, j = a.size() - 1;
+    bool iStop = false, jStop = false;
+    while(true) {
+        if (iStop == false && a[low] > a[i]) i++;
+        else iStop = true;
+
+        if (jStop == false && a[j] > a[low]) j--;
+        else jStop = true;
+        
+        if (iStop && jStop) {
+            cout << "\ni: " << i << "\n";
+            cout << "\nj: " << j << "\n";
+            cout << "Time to Change";
+
+            break;
+        }
+    }
+
+    return 10;
+}
+
 void sort(std::vector<int> arrayToBeSorted, int low, int high) {
     if (high <= low) return;
-    
+    int j = partition(arrayToBeSorted, low, high);
+    cout << "\n" << j << "\n";
 }
 
 void sort(std::vector<int> arrayToBeSorted) {
@@ -26,7 +56,7 @@ void sort(std::vector<int> arrayToBeSorted) {
     for(int i=0; i<arrayToBeSorted.size(); ++i)
     cout << arrayToBeSorted[i] << ' ';
 
-    sort(arrayToBeSorted, 0, sizeof(arrayToBeSorted) - 1);
+    sort(arrayToBeSorted, 0, arrayToBeSorted.size() - 1);
 }
 
 int main() {

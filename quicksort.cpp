60d9df814116b8479ba8bdb2ea07b2f1da5ff275
The Iliad <iliadmoosavi@gmail.com>
2017-10-31 03:29:47 -0400

    [QuickSort] - Shuffle array

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,4 +1,10 @@
 #include<stdio.h>
+#include <vector>
+using namespace std;
+#include <algorithm>    // std::random_shuffle
+#include <iostream>
+#include <random>
+
 
 void swap(int* a, int* b) {
     // return a
@@ -7,9 +13,26 @@ void swap(int* a, int* b) {
     *b = *a;
 }
 
+void sort(std::vector<int> arrayToBeSorted) {
+    std::random_device randomDevice;
+    std::mt19937 generator(randomDevice());
+    shuffle(arrayToBeSorted.begin(), arrayToBeSorted.end(), generator);
+
+    for(int i=0; i<arrayToBeSorted.size(); ++i)
+    cout << arrayToBeSorted[i] << ' ';
+}
+
 int main() {
-    printf("Test");
-    
+    printf("Test\n");
+
+    size_t size = 10;
+    std::vector<int> array(size);
+    for(int i=0; i<size; ++i) {
+        array[i] = i;
+    }
+
+    sort(array);
+
     int a = 10;
     int b = 5;
     swap(&a, &b);
